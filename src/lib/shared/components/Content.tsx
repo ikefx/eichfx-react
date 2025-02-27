314479e29bfb5034683da18054d7205e414f91ef
ikefx <n.eichelberger@gmail.com>
2025-02-27 06:59:54 -0600

    20250227

--- src/lib/shared/components/Content.tsx
+++ src/lib/shared/components/Content.tsx
@@ -3,37 +3,56 @@ import {Root} from "../../feature-root/components/Root.tsx";
 import {Reel} from "../../feature-reel/Reel.tsx";
 import {Breakdown} from "../../feature-breakdown/Breakdown.tsx";
 import {Research} from "../../feature-research/Research.tsx";
+import {Header} from "../../feature-root/components/Header.tsx";
+import {LeftNavigation} from "../../feature-root/components/LeftNavigation.tsx";
+import {NavigationProperties} from "../interfaces/interfaces.ts";
+import {useState} from "react";
 export function Content() {
+    const struct: NavigationProperties[] = [{
+        top: { title: '3D Studies', path: '/', },
+        items: [ { title: 'Demo Reel', path: '/reel', }, { title: 'Breakdown', path: '/breakdown', }, { title: 'Research', path: '/research', }, ]
+    }];
+    const [ nav ] = useState(struct);
     return (
         <>
-            <div className="col h-100">
-                <LocationProvider>
-                    <ErrorBoundary>
-                        <Router>
-                            <Route default component={Root}></Route>
-                            <Route path="/reel" component={Reel}></Route>
-                            <Route path="/breakdown" component={Breakdown}></Route>
-                            <Route path="/breakdown/terracotta" component={ () => (<Breakdown view="terracotta"></Breakdown>)}></Route>
-                            <Route path="/breakdown/hachiman" component={ () => (<Breakdown view="hachiman"></Breakdown>)}></Route>
-                            <Route path="/breakdown/rubiks" component={ () => (<Breakdown view="rubiks"></Breakdown>)}></Route>
-                            <Route path="/breakdown/tornado" component={ () => (<Breakdown view="tornado"></Breakdown>)}></Route>
-                            <Route path="/breakdown/pointstars" component={ () => (<Breakdown view="pointstars"></Breakdown>)}></Route>
-                            <Route path="/research" component={Research}></Route>
-                            <Route path="/research/aov" component={ () => (<Research view="aov"></Research>) }></Route>
-                            <Route path="/research/pointclouds" component={ () => (<Research view="pointclouds"></Research>) }></Route>
-                            <Route path="/research/volumetrics1" component={ () => (<Research view="volumetrics1"></Research>) }></Route>
-                            <Route path="/research/volumetrics2" component={ () => (<Research view="volumetrics2"></Research>) }></Route>
-                            <Route path="/research/subsurface" component={ () => (<Research view="subsurface"></Research>) }></Route>
-                            <Route path="/research/magiclights" component={ () => (<Research view="magiclights"></Research>) }></Route>
-                            <Route path="/research/fluidprimitives" component={ () => (<Research view="fluidprimitives"></Research>) }></Route>
-                            <Route path="/research/python1" component={ () => (<Research view="python1"></Research>) }></Route>
-                            <Route path="/research/magnetforce" component={ () => (<Research view="magnetforce"></Research>) }></Route>
-                            <Route path="/research/zbrushexport" component={ () => (<Research view="zbrushexport"></Research>) }></Route>
-                            <Route path="/research/matcaptext" component={ () => (<Research view="matcaptext"></Research>) }></Route>
-                            <Route path="/research/usgs" component={ () => (<Research view="usgs"></Research>) }></Route>
-                        </Router>
-                    </ErrorBoundary>
-                </LocationProvider>
+            <div className="d-flex flex-column root-pane">
+                <div className={'d-block d-xxl-none'}>
+                    <Header sections={nav}></Header>
+                </div>
+                <div className="d-flex h-100 position-relative overflow-hidden">
+                    <div className={'d-none d-xxl-flex'}>
+                        <LeftNavigation sections={nav}></LeftNavigation>
+                    </div>
+                    <div className="col h-100">
+                        <LocationProvider>
+                            <ErrorBoundary>
+                                <Router>
+                                    <Route default component={Root}></Route>
+                                    <Route path="/reel" component={Reel}></Route>
+                                    <Route path="/breakdown" component={Breakdown}></Route>
+                                    <Route path="/breakdown/terracotta" component={ () => (<Breakdown view="terracotta"></Breakdown>)}></Route>
+                                    <Route path="/breakdown/hachiman" component={ () => (<Breakdown view="hachiman"></Breakdown>)}></Route>
+                                    <Route path="/breakdown/rubiks" component={ () => (<Breakdown view="rubiks"></Breakdown>)}></Route>
+                                    <Route path="/breakdown/tornado" component={ () => (<Breakdown view="tornado"></Breakdown>)}></Route>
+                                    <Route path="/breakdown/pointstars" component={ () => (<Breakdown view="pointstars"></Breakdown>)}></Route>
+                                    <Route path="/research" component={Research}></Route>
+                                    <Route path="/research/aov" component={ () => (<Research view="aov"></Research>) }></Route>
+                                    <Route path="/research/pointclouds" component={ () => (<Research view="pointclouds"></Research>) }></Route>
+                                    <Route path="/research/volumetrics1" component={ () => (<Research view="volumetrics1"></Research>) }></Route>
+                                    <Route path="/research/volumetrics2" component={ () => (<Research view="volumetrics2"></Research>) }></Route>
+                                    <Route path="/research/subsurface" component={ () => (<Research view="subsurface"></Research>) }></Route>
+                                    <Route path="/research/magiclights" component={ () => (<Research view="magiclights"></Research>) }></Route>
+                                    <Route path="/research/fluidprimitives" component={ () => (<Research view="fluidprimitives"></Research>) }></Route>
+                                    <Route path="/research/python1" component={ () => (<Research view="python1"></Research>) }></Route>
+                                    <Route path="/research/magnetforce" component={ () => (<Research view="magnetforce"></Research>) }></Route>
+                                    <Route path="/research/zbrushexport" component={ () => (<Research view="zbrushexport"></Research>) }></Route>
+                                    <Route path="/research/matcaptext" component={ () => (<Research view="matcaptext"></Research>) }></Route>
+                                    <Route path="/research/usgs" component={ () => (<Research view="usgs"></Research>) }></Route>
+                                </Router>
+                            </ErrorBoundary>
+                        </LocationProvider>
+                    </div>
+                </div>
             </div>
         </>
     )
